6082add02ab9d42c406ee41b70b9a8143c9fc64f
chris.gruca@tlen.pl <chris.gruca@tlen.pl>
2019-10-03 12:09:24 +0200

    jej

---
+++ podstawy programowania/ZAJĘCIA 1/Pierwszy plik/Pierwszy plik/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Pierwszy_plik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // console.writeline("wsb");
+            //console.writeline("nowa linia");
+
+            //console.writeline("proszę podaj swoje imie");
+
+            ////typ string
+            //string name = console.readline();
+
+            //console.write("masz imię ");
+            //console.writeline(name);
+
+            ////typ integer
+
+            //int age = 20;
+
+            //    console.writeline(age);
+
+            //uint number = 10;
+
+
+            ////typ float
+            //float z = 14.55f;
+            ////+ konkatenacja
+            //console.writeline("zmienna z wynosi: " + z);
+
+            //Console.WriteLine("Program obliczający pole prostokąta");
+
+            //Console.Write("Podaj a: ");
+            //string aIn = Console.ReadLine();
+            //float a = float.Parse(aIn);
+            //Console.Write("Podaj b: ");
+            //string bIn = Console.ReadLine();
+            //float b = float.Parse(bIn);
+
+
+            //Console.WriteLine("Pole prostokąta wynosi: " + a*b);
+
+            //Console.WriteLine("Program obliczający pole prostokąta");
+            //Console.WriteLine("Podaj a: ");
+
+            //double x = Console.ReadLine();
+            //Console.Write("Podaj b: ");
+
+            //Console.WriteLine("Program obliczający pole prostokąta"); 
+
+            int j, k = 10, l;
+
+            //int zmienna = 10;
+
+            int zmienna = new int();
+
+
+
+            Console.ReadKey();
+           
+        }
+    }
+}
